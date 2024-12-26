4bdf33de4d5d4a3cf00db9916d88a3fafd47f0fa
Mehmet Aslan <aaslan.mehmet@hotmail.com>
2024-12-26 20:20:32 +0100

    memory management demos

--- misc/shell/rebuild_all.sh
+++ misc/shell/rebuild_all.sh
@@ -3,19 +3,38 @@
 thisPath=$(realpath "$0")
 thisDirPath=$(dirname "$thisPath")
 source "${thisDirPath}/util.sh"
-prjRootPath="${thisDirPath}/../.." # This is the root path of the project
+prjRootPath=$(realpath "${thisDirPath}/../..") # This is the root path of the project
 
 for demo in ${demos}
 do
-    echo "rebuilding ${demo}"
-    echo "========================================"
-    ${thisDirPath}/rebuild.sh "${prjRootPath}/${demo}"
-    if [ $? != 0 ]
+    macros=$(getMacros "${demo}")
+    if [ -z "$macros" ]
     then
-        echoerr "build ${demo} failure"
-        ungracefulExit
-    else
-        echo "build ${demo} success"
+        echo "rebuilding ${demo}"
         echo "========================================"
+        ${thisDirPath}/rebuild.sh "${prjRootPath}/${demo}"
+        if [ $? != 0 ]
+        then
+            echoerr "build ${demo} failure"
+            ungracefulExit
+        else
+            echo "build ${demo} success"
+            echo "========================================"
+        fi
+    else
+        for macro in ${macros}
+        do
+            echo "rebuilding ${demo} with ${macro}"
+            echo "========================================"
+            ${thisDirPath}/rebuild.sh ${prjRootPath}/${demo} ${macro}
+            if [ $? != 0 ]
+            then
+                echoerr "build ${demo} with ${macro} failure"
+                ungracefulExit
+            else
+                echo "build ${demo} with ${macro} success"
+                echo "========================================"
+            fi
+        done
     fi
 done
