497a359e6506c63908f5358be67f5e928af21fdc
Mehmet Aslan <aaslan.mehmet@hotmail.com>
2024-12-28 15:00:29 +0100

    6_mutex_challenge done

--- 6_mutex_challenge/README.md
+++ 6_mutex_challenge/README.md
@@ -4,6 +4,17 @@ Implementation in [app.c](./app.c).
 
 Challenge of [Introduction to RTOS Part 6 - Mutex](https://www.youtube.com/watch?v=I55auRpbiTs&list=PLEBQazB0HUyQ4hAPU1cJED6t3DU0h34bz&index=6)
 
+On the video he says this hack.
+Well new FreeRTOS version can detect the hack and assert.
+Below is their explanation:
+```
+ /* A task can only have an inherited priority if it holds the mutex.
+  * If the mutex is held by a task then it cannot be given from an
+  * interrupt, and if a mutex is given by the holding task then it must
+  * be the running state task. */
+```
+So binary semaphore used to solve the challenge
+
 ## Terminal Output
 
 ## Notes
--- 6_mutex_challenge/app.c
+++ 6_mutex_challenge/app.c
@@ -7,41 +7,78 @@
 #include "task.h"
 #include "uart.h"
 #include "util.h"
+#include "semphr.h"
 
 #define APP_BLINKY_TASK_PRIORITY (tskIDLE_PRIORITY + 1)
 #define APP_BLINKY_TASK_STACK_SIZE 1024
 
-static void app_blinky(void * pvParameters);
+static SemaphoreHandle_t app_mutex = NULL;
+
+void setup(void * pvParameters);
+static void app_blinkLED(void * pvParameters);
 
 void app_init(void) {
-    /* Start the two tasks as described in the comments at the top of this
-        * file. */
-    xTaskCreate(app_blinky,                 /* The function that implements the task. */
-                "blinky",                   /* The text name assigned to the task - for debug only as it is not used by the kernel. */
+    xTaskCreate(setup,                      /* The function that implements the task. */
+                "s",                        /* The text name assigned to the task - for debug only as it is not used by the kernel. */
                 APP_BLINKY_TASK_STACK_SIZE, /* The size of the stack to allocate to the task. */
-                NULL,                       /* The parameter passed to the task - not used in this simple case. */
+                NULL,                       /* The parameter passed to the task */
                 APP_BLINKY_TASK_PRIORITY,   /* The priority assigned to the task. */
                 NULL );                     /* The task handle is not required, so NULL is passed. */
+
     /* Start the tasks and timer running. */
     vTaskStartScheduler();
 
-    /* If all is well, the scheduler will now be running, and the following
-     * line will never be reached.  If the following line does execute, then
-     * there was insufficient FreeRTOS heap memory available for the idle and/or
-     * timer tasks	to be created.  See the memory management section on the
-     * FreeRTOS web site for more details.  NOTE: This demo uses static allocation
-     * for the idle and timer tasks so this line should never execute. */
     for( ; ; )
     {
     }
 }
 
-static void app_blinky( void * pvParameters ) {
-    /* Prevent the compiler warning about the unused parameter. */
+// simulating arduino environment
+void setup(void * pvParameters) {
     (void)pvParameters;
+    // on the video he says this hack
+    // well new FreeRTOS version can detect the hack and assert
+    // below is their explanation:
+            /* A task can only have an inherited priority if it holds the mutex.
+             * If the mutex is held by a task then it cannot be given from an
+             * interrupt, and if a mutex is given by the holding task then it must
+             * be the running state task. */
+    // app_mutex = xSemaphoreCreateMutex();
+
+    // so semaphore used to solve this challenge
+    app_mutex = xSemaphoreCreateBinary();
+    uint32_t delay = 1000;
+
+    xSemaphoreTake(app_mutex, 5);
+    printf("Enter delay: ");
+    uart0_readU32(&delay);
+    printf("\nDelay set to %d\n", (int)delay);
+
+    xTaskCreate(app_blinkLED,               /* The function that implements the task. */
+                "blink",                    /* The text name assigned to the task - for debug only as it is not used by the kernel. */
+                APP_BLINKY_TASK_STACK_SIZE, /* The size of the stack to allocate to the task. */
+                &delay,                     /* The parameter passed to the task */
+                APP_BLINKY_TASK_PRIORITY,   /* The priority assigned to the task. */
+                NULL );                     /* The task handle is not required, so NULL is passed. */
+
+    xSemaphoreTake(app_mutex, 5);
+    xSemaphoreGive(app_mutex);
+    vTaskDelete(NULL);
+}
+
+static void app_blinkLED( void * pvParameters ) {
+    if(app_mutex == NULL ||
+       pvParameters == NULL) {
+        printf("null checks fail");
+        while(1)
+            ;
+    }
     static uint8_t led_st = 0;
+    uint32_t delay = 100;
+    delay = *((uint32_t *) pvParameters);
+    xSemaphoreGive(app_mutex);
     for( ;; ) {
-        vTaskDelay( pdMS_TO_TICKS( 1000 ) );
+        vTaskDelay( pdMS_TO_TICKS( delay ) );
         led_st = !led_st;
         util_ledSet(led_st);
     }
