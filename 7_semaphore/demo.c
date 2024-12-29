53b4d3df4116f6b45426f4021f035d8f5280f8cb
Mehmet Aslan <aaslan.mehmet@hotmail.com>
2024-12-29 01:53:57 +0100

    7_semaphore refactored

--- 7_semaphore/demo.c
+++ 7_semaphore/demo.c
@@ -1,31 +1,26 @@
-/* Standard includes. */
-#include <stdio.h>
-#include <stdint.h>
-
 /* Kernel includes. */
 #include "FreeRTOS.h"
 #include "task.h"
-#include "uart.h"
-#include "util.h"
 #include "semphr.h"
-#include "string.h"
+#include "demo_hw.h"
+#include <string.h>
 
-#define APP_DEFAULT_TASK_PRIORITY (tskIDLE_PRIORITY + 1)
-#define APP_DEFAULT_TASK_STACK_SIZE 1024
+#define DEFAULT_TASK_PRIORITY (tskIDLE_PRIORITY + 1)
+#define DEFAULT_TASK_STACK_SIZE 1024
 
-#define APP_NUM_OF_PRINT_MSG_TASK (5u)
+#define NUM_OF_PRINT_MSG_TASK (5u)
 
-static SemaphoreHandle_t app_semphr = NULL;
+static SemaphoreHandle_t __semphr = NULL;
 
-void setup(void * pvParameters);
-static void app_printMsg(void * pvParameters);
+void _setup(void * pvParameters);
+static void _printMsg(void * pvParameters);
 
 void demo_init(void) {
-    xTaskCreate(setup,                      /* The function that implements the task. */
+    xTaskCreate(_setup,                      /* The function that implements the task. */
                 "S",                        /* The text name assigned to the task - for debug only as it is not used by the kernel. */
-                APP_DEFAULT_TASK_STACK_SIZE, /* The size of the stack to allocate to the task. */
+                DEFAULT_TASK_STACK_SIZE, /* The size of the stack to allocate to the task. */
                 NULL,                       /* The parameter passed to the task */
-                APP_DEFAULT_TASK_PRIORITY,   /* The priority assigned to the task. */
+                DEFAULT_TASK_PRIORITY,   /* The priority assigned to the task. */
                 NULL );                     /* The task handle is not required, so NULL is passed. */
 
     /* Start the tasks and timer running. */
@@ -37,42 +32,42 @@ void demo_init(void) {
 }
 
 // simulating arduino environment
-void setup(void * pvParameters) {
+void _setup(void * pvParameters) {
     (void)pvParameters;
-    char msg[] = "Hi from setup\'s stack\n";
-    char taskNames[APP_NUM_OF_PRINT_MSG_TASK][2] = {"A", "B", "C", "D", "E"};
+    char msg[] = "Hi from _setup\'s stack\n";
+    char taskNames[NUM_OF_PRINT_MSG_TASK][2] = {"A", "B", "C", "D", "E"};
     uint8_t i;
 
-    app_semphr = xSemaphoreCreateCounting(APP_NUM_OF_PRINT_MSG_TASK, 0);
+    __semphr = xSemaphoreCreateCounting(NUM_OF_PRINT_MSG_TASK, 0);
 
-    for(i = 0; i < APP_NUM_OF_PRINT_MSG_TASK; ++i) {
-        xTaskCreate(app_printMsg,               /* The function that implements the task. */
+    for(i = 0; i < NUM_OF_PRINT_MSG_TASK; ++i) {
+        xTaskCreate(_printMsg,               /* The function that implements the task. */
                     taskNames[i],                    /* The text name assigned to the task - for debug only as it is not used by the kernel. */
-                    APP_DEFAULT_TASK_STACK_SIZE, /* The size of the stack to allocate to the task. */
+                    DEFAULT_TASK_STACK_SIZE, /* The size of the stack to allocate to the task. */
                     msg,                     /* The parameter passed to the task */
-                    APP_DEFAULT_TASK_PRIORITY,   /* The priority assigned to the task. */
+                    DEFAULT_TASK_PRIORITY,   /* The priority assigned to the task. */
                     NULL );                     /* The task handle is not required, so NULL is passed. */
     }
 
-    for(i = 0; i < APP_NUM_OF_PRINT_MSG_TASK; ++i) {
-        while(xSemaphoreTake(app_semphr, 5) != pdTRUE)
+    for(i = 0; i < NUM_OF_PRINT_MSG_TASK; ++i) {
+        while(xSemaphoreTake(__semphr, 5) != pdTRUE)
             ;
     }
 
-    printf("setup done\n");
+    demo_hw_term_writeLine("_setup done");
     vTaskDelete(NULL);
 }
 
-static void app_printMsg( void * pvParameters ) {
-    if(app_semphr == NULL ||
+static void _printMsg( void * pvParameters ) {
+    if(__semphr == NULL ||
        pvParameters == NULL) {
-        printf("null checks fail");
+        demo_hw_term_writeLine("null checks fail");
         while(1)
             ;
     }
     char msg[100];
     strcpy(msg, (char *)pvParameters);
-    printf("%s", msg);
-    xSemaphoreGive(app_semphr);
+    demo_hw_term_printf("%s", msg);
+    xSemaphoreGive(__semphr);
     vTaskDelete(NULL);
 }
--- 7_semaphore_challenge/demo.c
+++ 7_semaphore_challenge/demo.c
@@ -1,69 +1,64 @@
-/* Standard includes. */
-#include <stdio.h>
-#include <stdint.h>
-
 /* Kernel includes. */
 #include "FreeRTOS.h"
 #include "task.h"
-#include "uart.h"
-#include "util.h"
 #include "semphr.h"
-#include "string.h"
 #include "ringbuffer.h"
+#include "demo_hw.h"
 
-#define APP_DEFAULT_TASK_PRIORITY (tskIDLE_PRIORITY + 1)
-#define APP_DEFAULT_TASK_STACK_SIZE 1024
+#define DEFAULT_TASK_PRIORITY (tskIDLE_PRIORITY + 1)
+#define DEFAULT_TASK_STACK_SIZE 1024
 
-#define APP_NUM_OF_PRODUCERS (5u)
-#define APP_RING_BUF_SIZE (32u)
+#define NUM_OF_PRODUCERS (5u)
+#define RING_BUF_SIZE (32u)
 
-void app_producerTask(void *pvParameters);
-void app_consumerTask(void *pvParameters);
+void _producerTask(void *pvParameters);
+void _consumerTask(void *pvParameters);
 
 typedef struct {
     char nameChar;
     uint8_t numOfSend;
-} app_producerHandle_T;
+} producerHandle_T;
 
-static app_producerHandle_T app_producerHandles[APP_NUM_OF_PRODUCERS];
+static producerHandle_T __producerHandles[NUM_OF_PRODUCERS];
 
-static ring_buffer_t app_ringBuf;
-static SemaphoreHandle_t app_ringBuf_mutex = NULL;
-static SemaphoreHandle_t app_ringBuf_semphr = NULL;
-static char app_ringBuf_buf[APP_RING_BUF_SIZE];
+static ring_buffer_t __ringBuf;
+static SemaphoreHandle_t __ringBuf_mutex = NULL;
+static SemaphoreHandle_t __ringBuf_semphr = NULL;
+static char __ringBuf_buf[RING_BUF_SIZE];
 
 void demo_init(void) {
     uint8_t i;
 
-    ring_buffer_init(&app_ringBuf, app_ringBuf_buf, sizeof(app_ringBuf_buf));
-    app_ringBuf_mutex = xSemaphoreCreateMutex();
-    app_ringBuf_semphr = xSemaphoreCreateCounting(APP_RING_BUF_SIZE, 0);
+    ring_buffer_init(&__ringBuf, __ringBuf_buf, sizeof(__ringBuf_buf));
+    __ringBuf_mutex = xSemaphoreCreateMutex();
+    __ringBuf_semphr = xSemaphoreCreateCounting(RING_BUF_SIZE, 0);
 
-    for(i = 0; i < APP_NUM_OF_PRODUCERS; ++i) {
-        char taskName[5];
-        sprintf(taskName, "%d", (int)i);
-        app_producerHandles[i].nameChar = '0' + i;
-        app_producerHandles[i].numOfSend = 0;
-        xTaskCreate(app_producerTask,            /* The function that implements the task. */
-                    taskName,                    /* The text name assigned to the task - for debug only as it is not used by the kernel. */
-                    APP_DEFAULT_TASK_STACK_SIZE, /* The size of the stack to allocate to the task. */
-                    &(app_producerHandles[i]),  /* The parameter passed to the task */
-                    APP_DEFAULT_TASK_PRIORITY,   /* The priority assigned to the task. */
-                    NULL );                      /* The task handle is not required, so NULL is passed. */
+    for(i = 0; i < NUM_OF_PRODUCERS; ++i) {
+        char taskName[2];
+        taskName[0] = '0' + i;
+        taskName[1] = '\0';
+        __producerHandles[i].nameChar = '0' + i;
+        __producerHandles[i].numOfSend = 0;
+        xTaskCreate(_producerTask,           /* The function that implements the task. */
+                    taskName,                /* The text name assigned to the task - for debug only as it is not used by the kernel. */
+                    DEFAULT_TASK_STACK_SIZE, /* The size of the stack to allocate to the task. */
+                    &(__producerHandles[i]), /* The parameter passed to the task */
+                    DEFAULT_TASK_PRIORITY,   /* The priority assigned to the task. */
+                    NULL );                  /* The task handle is not required, so NULL is passed. */
     }
 
-    xTaskCreate(app_consumerTask,            /* The function that implements the task. */
+    xTaskCreate(_consumerTask,               /* The function that implements the task. */
                 "A",                         /* The text name assigned to the task - for debug only as it is not used by the kernel. */
-                APP_DEFAULT_TASK_STACK_SIZE, /* The size of the stack to allocate to the task. */
+                DEFAULT_TASK_STACK_SIZE,     /* The size of the stack to allocate to the task. */
                 NULL,                        /* The parameter passed to the task */
-                APP_DEFAULT_TASK_PRIORITY,   /* The priority assigned to the task. */
+                DEFAULT_TASK_PRIORITY,       /* The priority assigned to the task. */
                 NULL );                      /* The task handle is not required, so NULL is passed. */
 
-    xTaskCreate(app_consumerTask,            /* The function that implements the task. */
+    xTaskCreate(_consumerTask,               /* The function that implements the task. */
                 "B",                         /* The text name assigned to the task - for debug only as it is not used by the kernel. */
-                APP_DEFAULT_TASK_STACK_SIZE, /* The size of the stack to allocate to the task. */
+                DEFAULT_TASK_STACK_SIZE,     /* The size of the stack to allocate to the task. */
                 NULL,                        /* The parameter passed to the task */
-                APP_DEFAULT_TASK_PRIORITY,   /* The priority assigned to the task. */
+                DEFAULT_TASK_PRIORITY,       /* The priority assigned to the task. */
                 NULL );                      /* The task handle is not required, so NULL is passed. */
 
     /* Start the tasks and timer running. */
@@ -73,53 +68,54 @@ void demo_init(void) {
     }
 }
 
-void app_consumerTask(void *pvParameters) {
+void _consumerTask(void *pvParameters) {
     // null pointer checks
-    if(app_ringBuf_mutex == NULL ||
-       app_ringBuf_semphr == NULL) {
+    if(__ringBuf_mutex == NULL ||
+       __ringBuf_semphr == NULL) {
         while(1)
             ;
     }
     char c;
 
     for( ; ; ) {
-        if(xSemaphoreTake(app_ringBuf_semphr, 0) == pdTRUE) {
+        if(xSemaphoreTake(__ringBuf_semphr, 0) == pdTRUE) {
             // wait until you get ringBuf access
-            while(xSemaphoreTake(app_ringBuf_mutex, 0) != pdTRUE)
+            while(xSemaphoreTake(__ringBuf_mutex, 0) != pdTRUE)
                 ;
-            // it is not possible that buffer is empty due app_ringBuf_semphr
-            ring_buffer_dequeue(&app_ringBuf, &c);
-            printf("%c\n", c);
-            xSemaphoreGive(app_ringBuf_mutex);
+            // it is not possible that buffer is empty due __ringBuf_semphr
+            ring_buffer_dequeue(&__ringBuf, &c);
+            demo_hw_term_writeChar(c);
+            demo_hw_term_writeChar('\n');
+            xSemaphoreGive(__ringBuf_mutex);
         }
     }
 }
 
-void app_producerTask(void *pvParameters) {
+void _producerTask(void *pvParameters) {
     // null pointer checks
     if(pvParameters == NULL ||
-       app_ringBuf_mutex == NULL ||
-       app_ringBuf_semphr == NULL) {
+       __ringBuf_mutex == NULL ||
+       __ringBuf_semphr == NULL) {
         while(1)
             ;
     }
 
-    app_producerHandle_T *producerHandlePtr = ((app_producerHandle_T *)pvParameters);
+    producerHandle_T *producerHandlePtr = ((producerHandle_T *)pvParameters);
 
     for( ; ; ) {
         if(producerHandlePtr->numOfSend >= 3) {
             // already achieved our goal
             break;
-        } if(xSemaphoreTake(app_ringBuf_mutex, 0) != pdTRUE) {
+        } if(xSemaphoreTake(__ringBuf_mutex, 0) != pdTRUE) {
             // do nothing if I can not get access
-        } else if(ring_buffer_is_full(&app_ringBuf)) {
+        } else if(ring_buffer_is_full(&__ringBuf)) {
             // if it is full just release
-            xSemaphoreGive(app_ringBuf_mutex);
+            xSemaphoreGive(__ringBuf_mutex);
         } else {
-            ring_buffer_queue(&app_ringBuf, producerHandlePtr->nameChar);
+            ring_buffer_queue(&__ringBuf, producerHandlePtr->nameChar);
             producerHandlePtr->numOfSend++;
-            xSemaphoreGive(app_ringBuf_semphr);
-            xSemaphoreGive(app_ringBuf_mutex);
+            xSemaphoreGive(__ringBuf_semphr);
+            xSemaphoreGive(__ringBuf_mutex);
         }
     }
 
